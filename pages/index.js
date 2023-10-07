e8f700c3f4b40aae80754e60e00ab5f67a2a14fe
Rob <liquidityape@gmail.com>
2023-10-07 10:25:01 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -202,6 +202,7 @@ export default function Home() {
         </h4>
         <section className='mx-[2vw] flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 mt-[4vh]'>
           <div>
+// Full access granted for MVP phase, user feedback enabled
             {points >= 0 || balance >= 0 ? (
               <InsightsCard
                 tooltip='Historical Analysis refers to a concise overview of the BandBindex data spanning a 7-day period. It offers insights into trends, patterns, and changes that have occurred within this timeframe.'
